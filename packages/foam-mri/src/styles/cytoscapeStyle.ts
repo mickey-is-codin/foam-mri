fa3be2401362dee1f19007d8d7605011fd896d18
Mickey Smith <smithmick14@gmail.com>
2020-11-24 21:08:46 -0600

    basic conditional rendering framework laid out

--- packages/foam-mri/src/styles/cytoscapeStyle.ts
+++ packages/foam-mri/src/styles/cytoscapeStyle.ts
@@ -1,15 +1,23 @@
 import { toHeatmapColors } from './colors';
+import { clipTo } from '../util/fp';
 
-const HEATMAP_RESOLUTION = 10;
-const heatmapColors = toHeatmapColors(HEATMAP_RESOLUTION);
-console.log('heatmap colors: ', heatmapColors);
+const BASE_NODE_COLOR = '#8b8b8b';
+const HEATMAP_RESOLUTION = 20;
+const heatmapColors = [BASE_NODE_COLOR, ...toHeatmapColors(HEATMAP_RESOLUTION)];
+
+const toBackgroundColor = (node: any) => {
+  const numSearchHits = node.data('searchHits').length;
+  console.log('node: ', node._private.data.id, ' searchHits: ', numSearchHits);
+  const clippedHits = clipTo(10)(numSearchHits);
+  return heatmapColors[clippedHits];
+};
 
 export const toBaseGraphStyle = () => {
   return [
     {
       selector: 'node',
       style: {
-        'background-color': '#8b8b8b',
+        'background-color': toBackgroundColor,
         'label': 'data(id)',
         'width': 10,
         'height': 10,
--- packages/foam-mri/src/util/fp.ts
+++ packages/foam-mri/src/util/fp.ts
@@ -1,1 +1,4 @@
 export const identity = (x: any) : any => x;
+export const clipTo = (max: number) => (x: number) => {
+  return x > max ? max : x;
+};
